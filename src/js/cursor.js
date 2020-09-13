c353ea6e9afad70cc47d5e0669725a616e695471
SG-WebDev <sgrobelny.contact@gmail.com>
2020-09-13 16:34:09 +0200

    Implemetation of motion-pointer package

---
+++ src/js/cursor.js
@@ -0,0 +1,11 @@
+const isDesktop = Math.min(window.screen.width, window.screen.height) >= 768 || navigator.userAgent.indexOf("Mobi") <= -1;
+const hoverElements = [
+    ".navTrigger",
+    ".nav__LangItem",
+    ".nav__Item",
+    ".section__PageTrigger",
+    ".portfolioList__Item"
+];
+if(isDesktop) {
+    motionPointerInit(hoverElements);
+}
